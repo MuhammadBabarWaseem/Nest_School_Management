f6866913befa5c7fc412382ca67ac6c9473a01f8
MuhammadBabarWaseem <mbabarwaseem@gmail.com>
2023-03-10 23:18:30 +0500

    First Commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { LessonModule } from './lesson/lesson.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    LessonModule,
+  ],
+})
+export class AppModule {}
---
+++ src/lesson/lesson.type.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+export class LessonType {
+  id: string;
+
+  name: string;
+
+  startDate: string;
+  
+  endDate: string;
+}

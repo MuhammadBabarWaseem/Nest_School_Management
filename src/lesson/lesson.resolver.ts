752cabb327a6f667cc80ddffbec99f772d285d48
MuhammadBabarWaseem <mbabarwaseem@gmail.com>
2023-03-12 00:59:40 +0500

    Added In Lesson Resolver

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,9 +1,11 @@
 /* eslint-disable prettier/prettier */
+import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
-import { Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
   @Query((returns) => LessonType)
   lesson() {
     return {
@@ -15,7 +17,11 @@ export class LessonResolver {
   }
 
   @Mutation(returns => LessonType)
-  createLesson() {
-// 
+   createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
   }
 }
